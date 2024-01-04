bfedb989e1c9ea83edc42bb38b41e1730efcedb3
Niraj Rana <71956273+nirajmohanrana@users.noreply.github.com>
2024-01-04 04:37:52 +0000

    check

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -13,7 +13,22 @@ const Dashboard = async () => {
     <div>
       Dashboard
       <br />
-      Hii {user.user_metadata.preferred_username}
+      Hii
+      {Object.entries(user).map(([key, value]) => {
+        function capitalizeString(str) {
+          return str
+            .replace(/([A-Z])/g, " $1")
+            .replace(/^./, str[0].toUpperCase())
+            .trim();
+        }
+
+        return (
+          <tr key={key}>
+            <td className="w-1/3">{capitalizeString(key)}:</td>
+            <td className="w-full font-bold">{value}</td>
+          </tr>
+        );
+      })}
     </div>
   );
 };
